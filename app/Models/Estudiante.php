5793f8d6ec133455e8e2b9cf4a95960de966dfb3
Ledin Alvarado <ledinalvarado18@gmail.com>
2026-01-29 13:46:50 -0600

    agregando migraciones y modelos nuevos

--- app/Models/Estudiante.php
+++ app/Models/Estudiante.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Profile;
 class Estudiante extends Model
 {
     use HasFactory;
     protected $fillable = ['nombre', 'facultad', 'correo', 'telefono'];
+
+    public function profile(){
+//        return $this->hasOne('App\Models\Profile');
+        return $this->hasOne(Profile::class);
+    }
 }
