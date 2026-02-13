537e9ddec6c6dc046184b0f52284a89d36146455
Ledin Alvarado <ledinalvarado18@gmail.com>
2026-02-13 13:31:13 -0600

    agregando el markdown para la configuracion de vuetify en el proyecto

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -15,6 +15,27 @@ import { Head } from '@inertiajs/vue3';
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">You're logged in!</div>
+<!--                    agregamos card de muestra-->
+
+                        <v-card
+                            class="mx-auto"
+                            color="surface-variant"
+                            image="https://cdn.vuetifyjs.com/docs/images/cards/dark-beach.jpg"
+                            max-width="440"
+                            subtitle="Take a walk down the beach"
+                            title="Evening sunset"
+                        >
+                            <template v-slot:actions>
+                                <v-btn
+                                    append-icon="mdi-chevron-right"
+                                    color="red-lighten-2"
+                                    text="Book Activity"
+                                    variant="outlined"
+                                    block
+                                ></v-btn>
+                            </template>
+                        </v-card>
+
                 </div>
             </div>
         </div>
